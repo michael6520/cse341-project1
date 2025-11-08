e354f977542539197a18f447b1e743b8b67110db
michael6520 <mm6520590@gmail.com>
2025-11-08 05:02:12 -0700

    week1

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongodb = require('./data/database');
+
+const app = express();
+app.set('json spaces', 2);
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`); });
+    }
+});

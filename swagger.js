08a299d3ff6056d882d0e719a50639cb82c686b2
michael6520 <mm6520590@gmail.com>
2025-11-15 16:37:28 -0700

    fix

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Contacts Api',
         description: 'Contacts Api'
     },
-    host: 'project1-fv51.onrender.com/',
+    host: 'project1-fv51.onrender.com',
     schemes: ['http','https']
 }
 const outputFile = './swagger.json';

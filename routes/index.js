aa3dd5f7153eca2c08f0f2160e6580725f355bdf
michael6520 <mm6520590@gmail.com>
2025-11-15 15:56:17 -0700

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => { res.send('Hello World'); });
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
+    res.send('Hello World');
+});
 
 router.use('/contacts', require('./contacts'));
 
--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.set('json spaces', 2);
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With. Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
